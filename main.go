1eafde0140f2b58d41a071fc22fa79d50a3bfa23
ledsouza <leandro.souza.159@gmail.com>
2024-12-30 15:46:16 -0300

    config: setting up the server

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = ":8080"
+	mux := http.NewServeMux()
+
+	var srv = &http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatalf("error in the server: %s", err)
+	}
+}

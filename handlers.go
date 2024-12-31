de4013e2c0f7164215596efc10ede30b9f8c3871
ledsouza <leandro.souza.159@gmail.com>
2024-12-31 20:07:12 -0300

    feat: added middleware and new endpoints

--- handlers.go
+++ handlers.go
@@ -1,9 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (c *apiConfig) handlerCountHit(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+
+	response := fmt.Sprintf(`
+	<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+	</html>`, c.fileserverHits.Load())
+	w.Write([]byte(response))
+}
+
+func (c *apiConfig) handlerResetCountHit(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	c.fileserverHits.Store(0)
+	response := "Hits reset"
+	w.Write([]byte(response))
 }
--- main.go
+++ main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	handlerFileServer := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handlerFileServer))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerCountHit)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerResetCountHit)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
